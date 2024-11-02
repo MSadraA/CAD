7e76eb9737b44363ca2a5faa7ab3931c770604e2
parsa alizadeh <p.alizadeh1383@gmail.com>
2024-11-02 17:21:50 +0330

    :)

--- ca2/Ram.v
+++ ca2/Ram.v
@@ -1,29 +1,29 @@
 module Ram 
-#(parameter BIT_SIZE = 16 ,
-parameter RAM_SIZE = 8;)
+#(
+    parameter BIT_SIZE = 16,
+    parameter RAM_SIZE = 8
+)
 (
     input clk,
     input rst,
     input ld,
-    [BIT_SIZE -1:0] input [0:RAM_SIZE] par_in,
-    [BIT_SIZE - 1:0] output [0:RAM_SIZE] par_out  
+    input [BIT_SIZE-1:0] par_in [0:RAM_SIZE-1],   // Input array of size RAM_SIZE, each element BIT_SIZE bits wide
+    output reg [BIT_SIZE-1:0] par_out [0:RAM_SIZE-1] // Output array of size RAM_SIZE, each element BIT_SIZE bits wide
 );
 
-integer i;
+    integer i;
 
     always @(posedge clk) begin
-        if(rst)
-            begin
-                for (i = 0; i < RAM_SIZE ; i = i + 1) begin
-                    par_out[i] <= BIT_SIZE'd0;
-                end
+        if (rst) begin
+            // Reset all outputs to 0
+            for (i = 0; i < RAM_SIZE; i = i + 1) begin
+                par_out[i] <= {BIT_SIZE{1'b0}};
             end
-        else
-            begin
-                if(ld)
-                    for (i = 0; i < RAM_SIZE ; i = i + 1) begin
-                    par_out[i] <= par_in[i];
-                end
+        end else if (ld) begin
+            // Load values from par_in to par_out
+            for (i = 0; i < RAM_SIZE; i = i + 1) begin
+                par_out[i] <= par_in[i];
             end
+        end
     end
 endmodule
