46ae2cdaa6af68b131b14ea2b6d4f895beae4445
parsa alizadeh <p.alizadeh1383@gmail.com>
2024-11-02 17:31:16 +0330

    :)

--- ca2/Ram.v
+++ ca2/Ram.v
@@ -7,8 +7,8 @@ module Ram
     input clk,
     input rst,
     input ld,
-    input [BIT_SIZE-1:0] par_in [0:RAM_SIZE-1],   // Input array of size RAM_SIZE, each element BIT_SIZE bits wide
-    output reg [BIT_SIZE-1:0] par_out [0:RAM_SIZE-1] // Output array of size RAM_SIZE, each element BIT_SIZE bits wide
+    input [(RAM_SIZE*BIT_SIZE)-1:0] par_in,      // Flattened input bus
+    output reg [(RAM_SIZE*BIT_SIZE)-1:0] par_out // Flattened output bus
 );
 
     integer i;
@@ -17,12 +17,12 @@ module Ram
         if (rst) begin
             // Reset all outputs to 0
             for (i = 0; i < RAM_SIZE; i = i + 1) begin
-                par_out[i] <= {BIT_SIZE{1'b0}};
+                par_out[i*BIT_SIZE +: BIT_SIZE] <= {BIT_SIZE{1'b0}};
             end
         end else if (ld) begin
             // Load values from par_in to par_out
             for (i = 0; i < RAM_SIZE; i = i + 1) begin
-                par_out[i] <= par_in[i];
+                par_out[i*BIT_SIZE +: BIT_SIZE] <= par_in[i*BIT_SIZE +: BIT_SIZE];
             end
         end
     end
