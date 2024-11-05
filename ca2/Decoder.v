b0126f6efec5832987b8d2e4532574f647809cde
parsa alizadeh <p.alizadeh1383@gmail.com>
2024-11-05 11:28:44 +0330

    L

--- ca2/Decoder.v
+++ ca2/Decoder.v
@@ -8,16 +8,23 @@ module Decoder
     input [(K * BIT) - 1:0] generated_addr,    
     output reg [SIZE - 1:0] out  
 );
-    integer i;
-    reg [BIT-1:0] addr_chunk;
     
+    // Combinational logic for output assignment
     always @(*) begin
-        out = {SIZE{1'b0}};
-        for (i = 0; i < K; i = i + 1) begin
-            // First extract the chunk into a fixed-width register
-            addr_chunk = generated_addr[BIT * (i + 1) - 1 : BIT * i];
-            // Then use that register as the array index
-            out[addr_chunk] = 1'b1;
-        end
+        out = {SIZE{1'b0}}; 
     end
+
+    // Generate block to create K separate combinational assignments
+    genvar g;
+    generate
+        for (g = 0; g < K; g = g + 1) begin : addr_decode
+            always @(*) begin
+                // Each generate block adds its decoded bit to the output
+                if (generated_addr[BIT * (g + 1) - 1 : BIT * g] < SIZE) begin
+                    out[generated_addr[BIT * (g + 1) - 1 : BIT * g]] = 1'b1;
+                end
+            end
+        end
+    endgenerate
+
 endmodule
