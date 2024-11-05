92b07fb59d3b0bb896a93ceb66677280ed08e797
MSadraA <mo.sadra.abbasi@gmail.com>
2024-11-05 11:16:59 +0330

    decoder

--- ca2/Decoder.v
+++ ca2/Decoder.v
@@ -1,20 +1,20 @@
-module multi_decoder #(parameter SIZE = 8, parameter BIT = $clog2(SIZE), parameter K = 4) (
-    input [(K * BIT) - 1:0] string,     // Concatenated input string of K chunks, each of BIT width
-    output reg [(K * SIZE) - 1:0] out   // Concatenated one-hot encoded outputs for each chunk
+module Decoder 
+#(
+parameter SIZE = 8 , 
+parameter K = 4,
+parameter BIT = $clog2(SIZE)
+) 
+(
+    input [(K * BIT) - 1:0] generated_addr,     // Concatenated input generated_addr of K chunks, each of BIT width
+    output reg [SIZE - 1:0] out   // Concatenated one-hot encoded outputs for each chunk
 );
 
     integer i; // Loop index
-
     always @(*) begin
-        // Loop through each chunk of the input string
-        for (i = 0; i < K; i = i + 1) begin
-            // Clear the current output segment to all 0's
-            out[i * SIZE +: SIZE] = {SIZE{1'b0}};
-            
-            // Decode each BIT chunk from string and set the corresponding output bit
-            if (string[i * BIT +: BIT] < SIZE) begin
-                out[i * SIZE + string[i * BIT +: BIT]] = 1'b1;
-            end
+        out = SIZE{1'b0};
+        // Loop through each chunk of the input generated_addr
+        for (i = 0; i < K ; i = i + 1) begin
+            out[generated_addr[BIT * (i+1) -1 : BIT * i]] = 1'b1;
         end
     end
 endmodule
--- ca2/tb.v
+++ ca2/tb.v
@@ -6,12 +6,13 @@ module tb();
     parameter K = 4;
     parameter SIZE = 16;
     parameter BIT = $clog2(K);
-    
+
     reg [$clog2(SIZE) - 1 : 0] in =  4'd14;
 
     wire [($clog2(SIZE) * K) - 1:0] num_out;
     wire [($clog2(SIZE) + $clog2(K)) * K - 1 : 0] out;
     wire [(SIZE * $clog2(K)) - 1:0] results;
+    wire [(K * SIZE) - 1:0] decoder_out;
 
     Generator #(.SIZE(SIZE) , .K(K))
     generator (
@@ -19,19 +20,26 @@ module tb();
         .num_out(num_out)
     );
 
-    Concat #(.SIZE(SIZE) , .K(K))
-    concat 
+    Decoder #(.SIZE(SIZE) , .K(K))
+    decoder
     (
-        .in(num_out),
-        .out(out)
+        .generated_addr(num_out) ,
+        .out(decoder_out)
     );
 
-    Array_selector #(.SIZE(SIZE) , .K(K))
-    array_selector
-    (
-        .inputs(out) ,
-        .results(results)
-    );
+    // Concat #(.SIZE(SIZE) , .K(K))
+    // concat 
+    // (
+    //     .in(num_out),
+    //     .out(out)
+    // );
+
+    // Array_selector #(.SIZE(SIZE) , .K(K))
+    // array_selector
+    // (
+    //     .inputs(out) ,
+    //     .results(results)
+    // );
 
     initial #10 $stop;
     
